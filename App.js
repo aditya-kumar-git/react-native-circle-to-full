3fc4b7c0ea7396dc658847c7eac8cf8f4cb717d8
Aditya Kumar <aditya1199kumar@gmail.com>
2020-05-13 01:55:22 +0530

    Added

--- App.js
+++ App.js
@@ -1,19 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	Animated,
+	StatusBar,
+	SafeAreaView,
+	Dimensions,
+} from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+	state = {
+		aniHeight: new Animated.Value(100),
+		aniWidth: new Animated.Value(100),
+		borderAnim: new Animated.Value(100),
+		backCol: new Animated.Value(0),
+	}
+	componentDidMount() {
+		console.log(Dimensions.get("screen").height, Dimensions.get("screen").width)
+	}
+	render() {
+		var backBack = this.state.backCol.interpolate({
+			inputRange: [0, 1, 2, 3],
+			outputRange: ["lightskyblue", "lightgreen", "red", "pink"],
+		})
+		return (
+			<View style={styles.container}>
+				<StatusBar barStyle="dark-content" />
+				<TouchableOpacity
+					onPress={() => {
+						Animated.parallel([
+							Animated.timing(this.state.aniHeight, {
+								duration: 1000,
+								toValue: Dimensions.get("screen").height,
+							}),
+							Animated.timing(this.state.aniWidth, {
+								duration: 1000,
+								toValue: Dimensions.get("screen").width,
+							}),
+							Animated.timing(this.state.borderAnim, {
+								duration: 1000,
+								toValue: 0,
+							}),
+							Animated.timing(this.state.backCol, {
+								duration: 1000,
+								toValue: 3,
+							}),
+						]).start()
+					}}
+				>
+					<Animated.View
+						style={{
+							backgroundColor: backBack,
+							height: this.state.aniHeight,
+							width: this.state.aniWidth,
+							borderRadius: this.state.borderAnim,
+						}}
+					></Animated.View>
+				</TouchableOpacity>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+})
